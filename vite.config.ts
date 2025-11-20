78e0f61ff94b730261f432549176d075e86a2349
Ko Nagase <geosanak@gmail.com>
2025-11-20 12:30:07 +0900

    Fix vite config base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "./",
+  base: "/chiban-viewer/",
   plugins: [react()],
   build: {
     outDir: 'build',
